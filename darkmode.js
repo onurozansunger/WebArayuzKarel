5205461249433e7aaef9ec00ff38d4aa117d387c
Melih <melihfakirullah@gmail.com>
2023-02-01 14:29:17 +0300

    darkmode.js düzenlemesi

--- darkmode.js
+++ darkmode.js
@@ -1,36 +1,40 @@
 const select = document.querySelector("#color-select");
-const collection = document.getElementsByClassName("mainheader");
-const collection2 = document.getElementsByClassName("li");
-console.log(collection);
+    const collection = document.getElementsByClassName("mainheader");
+    const collection2 = document.getElementsByClassName("li");
+    console.log(collection);
 
-// Sistemin temasını oku ve ona göre değiştir
-if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+    // Sistemin temasını oku ve ona göre değiştir
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
     select.value = "#2c2c2c";
 } else {
     select.value = "#00aeef";
 }
 
-document.body.style.backgroundColor = select.value;
-document.querySelector(".mainmenuframe").style.backgroundColor = select.value;
-
-collection[0].style.backgroundColor = select.value;
-collection[1].style.backgroundColor = select.value;
-collection[2].style.backgroundColor = select.value;
-collection[3].style.backgroundColor = select.value;
-collection[4].style.backgroundColor = select.value;
-
-collection2[0].style.backgroundColor = select.value;
-collection2[1].style.backgroundColor = select.value;
-collection2[2].style.backgroundColor = select.value;
-collection2[3].style.backgroundColor = select.value;
-collection2[4].style.backgroundColor = select.value;
-
-
-
-// Checkbox a göre değiştir
-select.addEventListener("change", function() {
     document.body.style.backgroundColor = select.value;
     document.querySelector(".mainmenuframe").style.backgroundColor = select.value;
+    <!--document.querySelector(".navbar").style.backgroundColor = select.value;
+     document.querySelector(".frame").style.backgroundColor = select.value;-->
+
+    collection[0].style.backgroundColor = select.value;
+    collection[1].style.backgroundColor = select.value;
+    collection[2].style.backgroundColor = select.value;
+    collection[3].style.backgroundColor = select.value;
+    collection[4].style.backgroundColor = select.value;
+
+    collection2[0].style.backgroundColor = select.value;
+    collection2[1].style.backgroundColor = select.value;
+    collection2[2].style.backgroundColor = select.value;
+    collection2[3].style.backgroundColor = select.value;
+    collection2[4].style.backgroundColor = select.value;
+
+
+
+    // Checkbox a göre değiştir
+    select.addEventListener("change", function() {
+    document.body.style.backgroundColor = select.value;
+    document.querySelector(".mainmenuframe").style.backgroundColor = select.value;
+    <!--document.querySelector(".navbar").style.backgroundColor = select.value;
+ document.querySelector(".frame").style.backgroundColor = select.value;-->
 
     collection[0].style.backgroundColor = select.value;
     collection[1].style.backgroundColor = select.value;
@@ -44,6 +48,3 @@ select.addEventListener("change", function() {
     collection2[3].style.backgroundColor = select.value;
     collection2[4].style.backgroundColor = select.value;
 });
-
-
-
