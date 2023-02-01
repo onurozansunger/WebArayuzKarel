61cc5d6555ae3a2a2b7e75b39eb79d6cde4892db
Melih <melihfakirullah@gmail.com>
2023-02-01 11:11:04 +0300

    tüm menü sayfaları eklendi

---
+++ darkmode.js
@@ -0,0 +1,43 @@
+const select = document.querySelector("#color-select");
+const collection = document.getElementsByClassName("mainheader");
+const collection2 = document.getElementsByClassName("li");
+console.log(collection);
+
+// Sistemin temasını oku ve ona göre değiştir
+if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+    select.value = "#2c2c2c";
+} else {
+    select.value = "#00aeef";
+}
+
+document.body.style.backgroundColor = select.value;
+document.querySelector(".mainmenuframe").style.backgroundColor = select.value;
+
+collection[0].style.backgroundColor = select.value;
+collection[1].style.backgroundColor = select.value;
+collection[2].style.backgroundColor = select.value;
+collection[3].style.backgroundColor = select.value;
+
+collection2[0].style.backgroundColor = select.value;
+collection2[1].style.backgroundColor = select.value;
+collection2[2].style.backgroundColor = select.value;
+collection2[3].style.backgroundColor = select.value;
+
+// Checkbox a göre değiştir
+select.addEventListener("change", function() {
+    document.body.style.backgroundColor = select.value;
+    document.querySelector(".mainmenuframe").style.backgroundColor = select.value;
+
+    collection[0].style.backgroundColor = select.value;
+    collection[1].style.backgroundColor = select.value;
+    collection[2].style.backgroundColor = select.value;
+    collection[3].style.backgroundColor = select.value;
+
+    collection2[0].style.backgroundColor = select.value;
+    collection2[1].style.backgroundColor = select.value;
+    collection2[2].style.backgroundColor = select.value;
+    collection2[3].style.backgroundColor = select.value;
+});
+
+
+
